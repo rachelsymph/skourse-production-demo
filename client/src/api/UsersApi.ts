e4c4893e1ce773c0e40a75c2b77864fa865cfacd
Rachel <rachel@symph.co>
2022-02-17 17:13:45 +0800

    update api environment variable

--- client/src/api/UsersApi.ts
+++ client/src/api/UsersApi.ts
@@ -1,3 +1,4 @@
+import dotenv from 'dotenv/config';
 import axios from 'axios';
 import { GenericResponse } from '../types/Generics.type';
 import { SaveUserResponse } from '../types/Users.type';
@@ -5,7 +6,8 @@ import { SaveUserResponse } from '../types/Users.type';
 async function saveUser(
   params: string
 ): Promise<GenericResponse<SaveUserResponse>> {
-  const baseUrl = process.env.BASE_URL || 'http://localhost:8000/api/users/';
+  const baseUrl =
+    process.env.REACT_APP_API_URL || 'http://localhost:8000/api/users';
 
   try {
     const result = await axios.post(baseUrl, params);
