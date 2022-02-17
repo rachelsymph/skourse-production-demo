d2d199bcb45f375bf7b74ace0dc1d87e13ded3a9
Rachel <rachel@symph.co>
2022-02-17 19:27:22 +0800

    add client side

--- app.js
+++ app.js
@@ -22,6 +22,9 @@ mongoose.connection.on('connected', () => {
   console.log('connected to DB');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 app.listen(port, () => {
   console.log(`server is listening to port ${port}`);
